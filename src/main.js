75d87989fecfa3a01a006891c787ddf415d7a1c0
vi <dev.facereal@gmail.com>
2018-12-03 22:36:54 +0700

    fin add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'jquery';
+import 'popper.js';
+
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
